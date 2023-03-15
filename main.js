246eca642495da632be362183b64f93b9e2f91e0
shukurullox0507 <shukurullox0507@gmail.com>
2023-03-15 16:03:17 +0500

    first commit in js

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+import { timeConvertor } from "./utils.js";
+const timer = document.querySelector(".main-time");
+const menu = document.querySelector(".main-menu");
+const startBtn = document.getElementById("startBtn");
